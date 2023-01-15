f7b6afe345cbb9483720eea2ab240d995d381f41
super amin <aminseiifi@gmail.com>
2023-01-16 01:50:50 +0330

    update shell

--- start.sh
+++ start.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-tmux new-session -d -s my_session 'source venv/bin/activate && celery -A traffic_disruptor beat'
-tmux new-session -d -s my_session 'source venv/bin/activate && celery -A traffic_disruptor worker -B'
+source venv/bin/activate
+pip install -r requirements.txt
+tmux new-session -d -s session_beat 'source venv/bin/activate && celery -A traffic_disruptor beat'
+tmux new-session -d -s session_worker 'source venv/bin/activate && celery -A traffic_disruptor worker -B'
